c71985b22ae58d99d63b7a82e7a537905aaea062
Collin McCabe <collinmichaelmccabe@gmail.com>
2018-03-14 21:52:45 -0400

    Update documentation for estimate functions

--- R/estimate_ens.R
+++ R/estimate_ens.R
@@ -1,8 +1,8 @@
 #' estimate_ens
 #'
 #' estimate_ens takes the outbreak time for a given network and compares this to the
-#'   relationship between group size and outbreak time on maximally to predict
-#'   effective network size
+#'   relationship between group size and outbreak time on maximally complete networks to
+#'   predict effective network size
 #'
 #' @param x the number of days that a disease took to break out through the observed network
 #' @param predict_model model output from either 'predict_SI_max' or 'predict_SIR_max'
