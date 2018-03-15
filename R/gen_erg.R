a924b77d4a16354edf048d631502a5cb95603c1b
Collin McCabe <collinmichaelmccabe@gmail.com>
2018-03-14 23:55:17 -0400

    Update documentation and dependencies for import and gen functions

--- R/gen_erg.R
+++ R/gen_erg.R
@@ -1,50 +1,59 @@
 #' gen_erg
 #'
-#' gen_erg generates a set of Erdos-Renyi exponential random graphs of size "samples", repeated for each combination of the number of "nodes" and "percents" of edges present, provided in the function call
+#' gen_erg generates a set of Erdos-Renyi exponential random graphs of size "samples",
+#'   repeated for each combination of the number of "nodes" and "percents" of edges
+#'   present, provided in the function call
 #'
-#' @param samples the integer number of independent random graphs to be generated for each combination of nodes and percents
-#' @param nodes an integer or vector of integers that indicate the size(s) of networks to be generated; if a list is supplied, graphs will be generated independently for each network size provided in the vector
-#' @param percents a numeric or vector of numerics that indicate the percent(s) of possible edges present in networks to be generated; if a list is supplied, graphs will be generated independently for each percent provided in the vector
+#' @param samples the integer number of independent random graphs to be generated for
+#'   each combination of nodes and percents
+#' @param nodes an integer or vector of integers that indicate the size(s) of networks
+#'   to be generated; if a list is supplied, graphs will be generated independently
+#'   for each network size provided in the vector
+#' @param percents a numeric or vector of numerics that indicate the percent(s) of
+#'   possible edges present in networks to be generated; if a list is supplied,
+#'   graphs will be generated independently for each percent provided in the vector
 #'
-#' @return list with 2 elements, the first of which (g) contains sociomatrices for each random graph, and the second of which (el) contains edgelists for those same graphs
+#' @return list with 2 elements, the first of which (g) contains sociomatrices for
+#'   each random graph, and the second of which (el) contains edgelists for those
+#'   same graphs
 #' @export
 #'
 #' @examples
 gen_erg <- function(samples, nodes, percents) {
 
-  gnm_g<-list(NA)
-  gnm_el<-list(NA)
+  gnm_g <- list(NA)
+  gnm_el <- list(NA)
 
   for (n in nodes) {
 
-    p_gnm_g<-list(NA)
-    p_gnm_el<-list(NA)
+    p_gnm_g <- list(NA)
+    p_gnm_el <- list(NA)
 
     for (p in percents) {
 
-      m = round((n^2 - n)*p)
-      g<-list(NA)
-      el<-list(NA)
+      m <- round((n^2 - n)*p)
+      g <- list(NA)
+      el <- list(NA)
 
-      i=1
+      i <- 1
       while(i < (samples + 1)) {
-        g[[i]] <- sample_gnm(n, m, directed=FALSE, loops=FALSE)
-        if(vertex_connectivity(g[[i]], checks = TRUE) > 0) {
+        g[[i]] <- igraph::sample_gnm(n, m, directed = FALSE, loops = FALSE)
+        if(igraph::vertex_connectivity(g[[i]], checks = TRUE) > 0) {
           el[[i]] <- unique(get.edgelist(g[[i]]))
-          el[[i]] <- cbind(el[[i]],weight=1)
-          el[[i]] <- cbind(el[[i]],n_nodes=n)
-          el[[i]] <- cbind(el[[i]],n_edges=m)
-          i = i+1
+          el[[i]] <- cbind(el[[i]], weight = 1)
+          el[[i]] <- cbind(el[[i]], n_nodes = n)
+          el[[i]] <- cbind(el[[i]], n_edges = m)
+          i <- i + 1
         }
       }
 
-      x=p*100
-      p_gnm_g[[x]]<-g
-      p_gnm_el[[x]]<-el
+      x <- p * 100
+      p_gnm_g[[x]] <- g
+      p_gnm_el[[x]] <- el
     }
 
-    gnm_g[[n]]<-p_gnm_g
-    gnm_el[[n]]<-p_gnm_el
+    gnm_g[[n]] <- p_gnm_g
+    gnm_el[[n]] <- p_gnm_el
   }
 
   return(list(g = gnm_g, el = gnm_el))
--- R/import_emp.R
+++ R/import_emp.R
@@ -23,11 +23,11 @@ import_emp <- function(folder) {
   for(i in 1:length(input_names)) {
     tmp <- read.csv(paste(folder, input_names[i], sep = ""), header = FALSE)
 
-    emp_g[[i]] <- graph_from_edgelist(as.matrix(tmp[,1:2]), directed = FALSE)
+    emp_g[[i]] <- igraph::graph_from_edgelist(as.matrix(tmp[,1:2]), directed = FALSE)
 
-    emp_el[[i]] <- cbind(unique(get.edgelist(emp_g[[i]])), weight = 1, n_nodes = c(vcount(emp_g[[i]]), rep(NA, ecount(emp_g[[i]]) / 2 - 1)), n_edges = c(ecount(emp_g[[i]]) / 2, rep(NA, ecount(emp_g[[i]]) / 2 - 1)))
+    emp_el[[i]] <- cbind(unique(igraph::get.edgelist(emp_g[[i]])), weight = 1, n_nodes = c(igraph::vcount(emp_g[[i]]), rep(NA, igraph::ecount(emp_g[[i]]) / 2 - 1)), n_edges = c(igraph::ecount(emp_g[[i]]) / 2, rep(NA, igraph::ecount(emp_g[[i]]) / 2 - 1)))
 
-    emp_el_w[[i]] <- cbind(unique(get.edgelist(emp_g[[i]])), weight = NA, n_nodes = c(vcount(emp_g[[i]]), rep(NA, ecount(emp_g[[i]]) / 2 - 1)), n_edges = c(ecount(emp_g[[i]]) / 2, rep(NA, ecount(emp_g[[i]]) / 2 - 1)))
+    emp_el_w[[i]] <- cbind(unique(igraph::get.edgelist(emp_g[[i]])), weight = NA, n_nodes = c(igraph::vcount(emp_g[[i]]), rep(NA, igraph::ecount(emp_g[[i]]) / 2 - 1)), n_edges = c(igraph::ecount(emp_g[[i]]) / 2, rep(NA, igraph::ecount(emp_g[[i]]) / 2 - 1)))
 
     for (j in 1:emp_el_w[[i]][1,5]) {
       emp_el_w[[i]][j,3] <- tmp[which(tmp[,1] == emp_el_w[[i]][j,1] & tmp[,2] == emp_el_w[[i]][j,2]), 3]
