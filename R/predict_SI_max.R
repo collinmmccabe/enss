fce7bf4a608c0991dac06a5ea314ea6ce34aa92b
Collin McCabe <collinmichaelmccabe@gmail.com>
2018-03-15 00:11:22 -0400

    Update documentation and dependencies for the predicct functions

--- R/predict_SI_max.R
+++ R/predict_SI_max.R
@@ -1,9 +1,14 @@
 #' predict_SI_max
 #'
-#' @param sim_output
-#' @param n_min
+#' predict_SI_max generates a model II regression prediction for the relationship between
+#'   outbreak time and maximally complete network size for SI transmission simulations
 #'
-#' @return
+#' @param sim_output simulation output from any of the sim_SI functions (including
+#'   sim_SI_unif and sim_SI_w) in enss using maximally complete graphs
+#' @param n_min the minimum network size in the set of maximally complete graphs used
+#'   to generate sim_output in the first argument
+#'
+#' @return lmodel2-type regression model object
 #' @export
 #'
 #' @examples
@@ -18,7 +23,7 @@ predict_SI_max <- function(sim_output, n_min) {
     SI_max_output <- rbind(SI_max_output, tmp)
   }
 
-  SImod2 <- lmodel2(log(n) ~ days, data = SI_max_output, range.y = "relative", range.x = "relative")
+  SImod2 <- lmodel2::lmodel2(log(n) ~ days, data = SI_max_output, range.y = "relative", range.x = "relative")
 
   return(SImod2)
 
